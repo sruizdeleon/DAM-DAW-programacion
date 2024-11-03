3718e4638679b3ba782905870b1e30e11099a4c6
sruizdeleon <ruizdeleonsergio8@gmail.com>
2024-11-03 02:10:05 +0100

    subiendo ejercicios de clase y entregas

---
+++ javaBasics/src/com/ejercicios/clase3010/Ejercicio04.java
@@ -0,0 +1,17 @@
+package com.ejercicios.clase3010;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+        // Enunciado:
+        // Escribe un programa que muestre un menú de opciones para convertir unidades:
+        // 1. Kilómetros a millas
+        // 2. Grados Celsius a Fahrenheit
+        // 3. Kilogramos a libras
+        // 4. Salir
+        // El programa debe solicitar al usuario la cantidad a convertir y mostrar el resultado.
+        // Usa un bucle for para repetir el menú hasta que el usuario elija salir.
+		
+	}
+
+}
